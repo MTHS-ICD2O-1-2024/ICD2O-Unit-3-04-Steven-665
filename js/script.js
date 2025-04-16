b5679e1f64cff6ba6efd27ec555e93f23f08a868
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-16 17:13:55 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+
+/**
+* This function calculates Fahrenheit into Celsius.
+*/
+function calculateFahrenheitToCelsius () {
+ // input
+ const fahrenheit  = parseFloat(document.getElementById('fahrenheit').value)
+ const quantification = 32
+
+ // process
+ const celsius = (fahrenheit - quantification) * 5/9.
+
+
+ // output
+ document.getElementById('celsius').innerHTML = 'celsius is: ' + celsius.toFixed(2) + ' °'
+}
